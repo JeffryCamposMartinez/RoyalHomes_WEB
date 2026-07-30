8cd58dbde9426a8b6e64d72c9e2f987c15dedeb6
JeffryCampos <jeffry.campos@virginiogomez.cl>
2026-07-30 14:01:33 -0400

    Limpiar scripts huerfanos y archivos basura

--- backend/check_admin_cart.js
+++
@@ -1,9 +0,0 @@
-const db = require('./config/db');
-
-async function checkCart() {
-  const [users] = await db.query("SELECT id, nombre, email, carrito FROM usuarios WHERE id = 1");
-  console.log(users);
-  process.exit(0);
-}
-
-checkCart();
